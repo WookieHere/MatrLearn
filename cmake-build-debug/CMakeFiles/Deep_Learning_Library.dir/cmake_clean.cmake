b166701a26e5aaa2276f36f4e3711d452e8d8b50
lukenasby <l_nasby@mac.com>
2022-02-07 01:08:42 -0500

    Initial creation of classes

---
+++ cmake-build-debug/CMakeFiles/Deep_Learning_Library.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Deep_Learning_Library.dir/Connection.cpp.o"
+  "CMakeFiles/Deep_Learning_Library.dir/Layer.cpp.o"
+  "CMakeFiles/Deep_Learning_Library.dir/testing_driver.cpp.o"
+  "Deep_Learning_Library"
+  "Deep_Learning_Library.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Deep_Learning_Library.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
